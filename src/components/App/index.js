1db10b986af26de98295649233d71442d732ac29
Risabh Kumar <rahulpsd18@gmail.com>
2018-11-29 21:53:30 +0530

    Added Authentication and Authorization HOCs

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,20 +10,26 @@ import AccountPage from "../Account";
 import AdminPage from "../Admin";
 
 import * as ROUTES from "../../constants/routes";
+import { withFirebase } from "../Firebase";
+import { withAuthentication  } from "../Session";
 
 const App = () => (
   <Router>
     <div>
       <Navigation />
+
       <hr />
+
+      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
+      {/* <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} /> */}
       <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route exact path={ROUTES.HOME} component={HomePage} />
+      {/* <Route exact path={ROUTES.HOME} component={HomePage} />
       <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route exact path={ROUTES.ADMIN} component={AdminPage} /> */}
     </div>
   </Router>
 );
 
-export default App;
+export default withAuthentication(App);
+
+// https://www.robinwieruch.de/complete-firebase-authentication-react-tutorial/#react-firebase-protected-routes
